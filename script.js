1fb0eac6120b5bb7ecf81ef4010aac537cf19daa
WewenGit <owenbons@gmail.com>
2023-12-07 22:57:09 +0100

    pour git

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var text = ["Malheur ! Vous avez été kidnappé ! \n Mais au vu du son quasi-inexistant produit par le véhicule dans lequel vous avez été chargé,il s'agirait plutôt d'un greennapping... \n Au vu de l'autonomie de ce genre de véhicule, vous savez que vous en aurez pour maximum 1h30... avant le prochain plein."
+var text = ["Malheur ! Vous avez été kidnappé ! \n Mais  à entendre le son quasi-inexistant produit par le véhicule dans lequel vous avez été chargé,il s'agirait plutôt d'un greennapping... \n Au vu de l'autonomie de ce genre de véhicule, vous savez que vous en aurez pour maximum 1h30... avant le prochain plein."
 , "Text 1", "Text 2", "Text 3", "Text 4"];
 var whereText = 0;
 
