dbdb33cc7dc01ffa5b36285b7aa0f84914eaf577
lulug <zeyzer37@gmail.com>
2023-12-08 06:33:35 +0100

    pas grand chose non plus

--- script.js
+++ script.js
@@ -6,11 +6,32 @@ var text = [
     "Vous alliez tranquillement échouer à obtenir le titre de Champion du Monde de Scrabble quand ce type bizarre au crâne luisant vous a attrapé.",
     "La seule chose dont vous vous souvenez c'est de vous être réveillé dans le coffre d'une voiure électrique, ligoté de la tête aux pieds. Vous n\'allez pas rester comme ça sans rien faire !",
     "Le coffre a l'air d'être ouvrable de l'intérieur... Que voulez-vous faire ?",
+    "Vous commencez à tirer sur vos liens. Vous pourriez peut-être les desserrer ainsi...Mais malheureusement, vous êtes vieux.",
+    "Après à peu près 35 minutes à souffrir de vos rhumatismes sur un pauvre noeud d'arrêt, la voiture de votre ravisseur marque le sien. Vous entendez des pas autour du véhicule. Vous êtes arrivé.",
+    "Le coffre s'ouvre sur la nuit noire. Le visage de votre greennappeur éclairé par la faible lumière du coffre vous apparaît maintenant clairement. Chauve, le regard malicieux, une barbe grisonnante et des lunettes roses, vous êtes face au sosie parfait de Pascal Obispo.",
+    "Il vous soulève sans problème (il est très musclé), et entre dans un bunker souterrain.",
+    "Il parcourt un dédale de couloirs incompréhensible et vous jette avec une forme surhumaine dans une pièce sombre qu'il referme sur vous. Ses pas s'éloignent peu à peu...",
+    "Que voulez-vous faire ?",
+    "Vous cherchez dans la pièce à la recherche d'un moyen d'en sortir.",
+    "Vous trouvez une inscription sur un mur :\nLe dérèglement climatique n'est pas une affaire si urgente",
+    "Qu'est-ce que c'est que ce truc ? Le précédent otage était-il devenu fou avant de finir en saucisse ?",
+    "Vous regardez autour de vous. La pièce comporte trois issues. L'une d'entre elle est celle par laquelle vous êtes entré, et elle est verrouillée. Quant aux deux autres... elles n'en ont pas l'air... Vous remarquez d'autres inscriptions dessus...",
+    "Quelle porte choisissez-vous ?",
+
 ];
 var whereText=0;
 
 function get(name) {
     return document.getElementById(name);
+}
+
+function point(depth)
+{
+    var textBox = get("text-box");
+    setTimeout(function() {
+            textBox.innerHTML+=".";
+            if(depth > 0) point(depth-1);
+        }, 1000);
 }
 
 function changeImage() {
@@ -31,15 +52,12 @@ function nextText() {
     
     nextButton.innerHTML="Suivant";
     if (whereText==7) {
-        var bouton1 = get("bouton-choix1");
-        var bouton2 = get("bouton-choix2");
-        var nextButton = get("next-button");
-        bouton1.innerHTML = "Tirer sur vos liens";
-        bouton2.innerHTML = "Attendre";
-        bouton1.style.display="block";
-        bouton2.style.display="block";
-        nextButton.style.display="none";
-    } else {
+        choix1();
+    } else if(whereText==13)
+    {
+        choix2();
+    }
+     else {
         if (whereText < text.length) {
             textBox.innerHTML = text[whereText++];
         } else {
@@ -61,47 +79,86 @@ function develop(){
     }
 }
 
-function choix1(){
+function choix1() {
+    var bouton1 = get("bouton-choix1");
+    var bouton2 = get("bouton-choix2");
+    var nextButton = get("next-button");
+    bouton1.innerHTML = "Tirer sur vos liens";
+    bouton2.innerHTML = "Attendre";
+    bouton1.style.display="block";
+    bouton2.style.display="block";
+    nextButton.style.display="none";
+}
+
+function choix1_1(){
     var textBox = get("text-box");
     var bouton1 = get("bouton-choix1");
     var bouton2 = get("bouton-choix2");
     var nextButton = get("next-button");
-    if (whereText==7) {
-        text.push("Vous commencez à tirer sur vos liens. Vous pourriez peut-être les desserrer ainsi...Mais malheureusement, vous êtes vieux.");
-        text.push("Après à peu près 35 minutes à souffrir de vos rhumatismes sur un pauvre noeud d'arrêt, la voiture de votre ravisseur marque le sien. Vous entendez des pas autour du véhicule. Vous êtes arrivé.");
-        text.push("Le coffre s'ouvre sur la nuit noire. Le visage de votre greennappeur éclairé par la faible lumière du coffre vous apparaît maintenant clairement. Chauve, le regard malicieux, une barbe grisonnante et des lunettes roses, vous êtes face au sosie parfait de Pascal Obispo.");
-        bouton1.style.display="none";
-        bouton2.style.display="none";
-        nextButton.style.display="block";
-        textBox.innerHTML = text[whereText++];
-    }
+    bouton1.style.display="none";
+    bouton2.style.display="none";
+    nextButton.style.display="block";
+    textBox.innerHTML = text[whereText++];
 }
 
-function point(depth)
-{
+function choix1_2(){
     var textBox = get("text-box");
+    var bouton1 = get("bouton-choix1");
+    var bouton2 = get("bouton-choix2");
+    var nextButton = get("next-button");
+    bouton1.style.display="none";
+    bouton2.style.display="none";
+    nextButton.innerHTML="Arrêter d'attendre";
+    nextButton.style.display="block";
+    textBox.innerHTML="Vous attendez";
+    point(2);
+    whereText--;
     setTimeout(function() {
-            textBox.innerHTML+=".";
-            if(depth > 0) point(depth-1);
-        }, 1000);
+        textBox.innerHTML="Vous comprenez bien que ça ne servira à rien, pas vrai ?";
+    }, 90000);
 }
 
-function choix2(){
+
+
+
+function choix2()
+{
+    var bouton1 = get("bouton-choix1");
+    var bouton2 = get("bouton-choix2");
+    var nextButton = get("next-button");
+    bouton1.innerHTML = "Attendre";
+    bouton2.innerHTML = "Chercher un moyen de sortir";
+    bouton1.style.display="block";
+    bouton2.style.display="block";
+    nextButton.style.display="none";
+    bouton1.onclick = choix2_1;
+    bouton2.onclick = choix2_2;
+}
+
+function choix2_1() {
     var textBox = get("text-box");
     var bouton1 = get("bouton-choix1");
     var bouton2 = get("bouton-choix2");
     var nextButton = get("next-button");
-    
-    if (whereText==7) {
-        bouton1.style.display="none";
-        bouton2.style.display="none";
-        nextButton.innerHTML="Arrêter d'attendre";
-        nextButton.style.display="block";
-        textBox.innerHTML="Vous attendez";
-        point(2);
-        whereText=6;
-        setTimeout(function() {
-            textBox.innerHTML="Vous comprenez bien que ça ne servira à rien, pas vrai ?";
-        }, 90000);
-    }
+    bouton1.style.display="none";
+    bouton2.style.display="none";
+    nextButton.innerHTML="Arrêter d'attendre";
+    nextButton.style.display="block";
+    textBox.innerHTML="Vous attendez";
+    point(2);
+    whereText--;
+    setTimeout(function() {
+        alert("Vous êtes mort de viellesse...")
+    }, 900000);
+}
+
+function choix2_2() {
+    var textBox = get("text-box");
+    var bouton1 = get("bouton-choix1");
+    var bouton2 = get("bouton-choix2");
+    var nextButton = get("next-button");
+    bouton1.style.display="none";
+    bouton2.style.display="none";
+    nextButton.style.display="block";
+    textBox.innerHTML = text[whereText++];
 }
